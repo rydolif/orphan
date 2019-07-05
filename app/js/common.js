6e6c96225c9b2a6ba5b36fddd0315755383c175b
rudolif <rudolifrudolif@gmail.com>
2019-07-06 01:41:08 +0200

    index + program commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,83 @@
 $(function() {
 
+//---------------------------video-------------------------------
+var youtube = $('.youtube');
+  $.each(youtube, function(index, el) {
+    var source = "https://img.youtube.com/vi/"+ $(el).data('embed') +"/sddefault.jpg";
+    var image = new Image();
+    image.src = source;
+    image.addEventListener( "load", function() {
+      youtube[ index ].append( image );
+    }( index ) );
+
+  $(el).on('click', function() {
+    var iframe = document.createElement( "iframe" );
+
+    iframe.setAttribute( "frameborder", "0" );
+    iframe.setAttribute( "allowfullscreen", "" );
+    iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ $(this).data('embed') +"?rel=0&showinfo=0&autoplay=1" );
+    $(this).innerHTML = "";
+    $(this).append( iframe );
+    $(this).find('.play-button').hide();
+  });
+});
+
+//---------------------------jScrollPane-------------------------------
+  $(window).resize(function(event) {
+    $('.sos__content_text').jScrollPane();
+  });
+
+
+//---------------------------slider-------------------------------
+  var swiper = new Swiper('.history__slider', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.history__next',
+      prevEl: '.history__prev',
+    },
+  });
+
+  var swiper = new Swiper('.sos__slider', {
+    loop: true,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.sos__next',
+      prevEl: '.sos__prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">0' + (index + 1) + '</span>';
+      },
+    },
+  });
+
+  var swiper = new Swiper('.realized__slider', {
+    slidesPerView: 2.5,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.realized__next',
+      prevEl: '.realized__prev',
+    },
+  });
+
+  var swiper = new Swiper('.team__slider', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.team__next',
+      prevEl: '.team__prev',
+    },
+  });
+
 //------------------------------гамбургер-----------------------------
   $('.hamburger').click(function() {
     $(this).toggleClass('hamburger--active');
@@ -9,55 +87,6 @@ $(function() {
 
 //-------------------------------попандер---------------------------------------
   $('.modal').popup({transition: 'all 0.3s'});
-
-//------------------------------------form-------------------------------------------
-  $('input[type="tel"]').mask('+0 (000) 000-00-00');
-
-  jQuery.validator.addMethod("phoneno", function(phone_number, element) {
-     return this.optional(element) || phone_number.match(/\+[0-9]{1}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}/);
-  }, "Введите Ваш телефон");
-
-  $(".form").each(function(index, el) {
-    $(el).addClass('form-' + index);
-
-    $('.form-' + index).validate({
-      rules: {
-        phone: {
-          required: true,
-          phoneno: true
-        },
-        name: 'required',
-      },
-      messages: {
-        name: "Введите Ваше имя",
-        phone: "Введите Ваш телефон",
-      },
-      submitHandler: function(form) {
-        var t = {
-          name: jQuery('.form-' + index).find("input[name=name]").val(),
-          phone: jQuery('.form-' + index).find("input[name=phone]").val(),
-          subject: jQuery('.form-' + index).find("input[name=subject]").val()
-        };
-        ajaxSend('.form-' + index, t);
-      }
-    });
-
-  });
-
-  function ajaxSend(formName, data) {
-    jQuery.ajax({
-      type: "POST",
-      url: "sendmail.php",
-      data: data,
-      success: function() {
-        $(".modal").popup("hide");
-        $("#thanks").popup("show");
-        setTimeout(function() {
-          $(formName).trigger('reset');
-        }, 2000);
-      }
-    });
-  }
 
 //----------------------------------------fixed----------------------------------
   $(window).scroll(function(){
@@ -75,38 +104,7 @@ $(function() {
       var id  = $(this).attr('href'),
           top = $(id).offset().top;
       $('body,html').animate({scrollTop: top - 60}, 'slow', 'swing');
-  //--------------------закриття меню при кліку на ссилку якоря--------------------
-     // $('.hamburger').removeClass('hamburger--active');
-     // $('.header-menu').removeClass('header-menu');
-     // $('.header--active').removeClass('header--active');
-     // $('.nav--active').removeClass('nav--active');
-
   });
-
-  // //-------------------------------анімація цифр---------------------------------------
-  //   var show = true;
-  //   var countbox = ".about-statistics__container";
-  //   $(window).on("scroll load resize", function () {
-  //       if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-  //       var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-  //       var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-  //       var w_height = $(window).height(); // Высота окна браузера
-  //       var d_height = $(document).height(); // Высота всего документа
-  //       var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-  //       if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-  //           $('.about-statistics__item h3').spincrement({
-  //               thousandSeparator: "",
-  //               duration: 2000
-  //           });
-  //           show = false;
-  //       }
-  //   });
 
   
 });
-
-//----------------------------------------preloader----------------------------------
-
-  // $(window).on('load', function(){
-  //   $('.preloader').delay(1000).fadeOut('slow');
-  // });
